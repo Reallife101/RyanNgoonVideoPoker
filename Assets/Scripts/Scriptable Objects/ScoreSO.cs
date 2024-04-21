ffffdde0bda7b572bb8daf3bbc592cb12707e3d9
Reallife101 <ryan.ngoon@gmail.com>
2024-04-20 22:41:06 -0700

    add start of Hand Manager

---
+++ Assets/Scripts/Scriptable Objects/ScoreSO.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+
+namespace VideoPoker
+{
+    [CreateAssetMenu(fileName = "New Score", menuName = "Cards/Score SO")]
+    public class ScoreSO : ScriptableObject
+    {
+        public float payout;
+
+    }
+}
