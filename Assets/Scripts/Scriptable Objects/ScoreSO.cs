5be7234245e19974f9cb52a3c53b9eb3b02b6e88
Reallife101 <ryan.ngoon@gmail.com>
2024-04-21 00:10:45 -0700

    Fix name mismatch

--- Assets/Scripts/Scriptable Objects/ScoreSO.cs
+++ Assets/Scripts/Scriptable Objects/ScoreSO.cs
@@ -4,19 +4,12 @@ using UnityEngine;
 
 namespace VideoPoker
 {
-    public delegate void ScoreLambda(List<Card> value);
-
     [CreateAssetMenu(fileName = "New Score", menuName = "Cards/Score SO")]
     public class ScoreSO : ScriptableObject
     {
         public float payout;
 
-        public ScoreLambda scoreLambda;
 
-        public void InvokeLambda(List<Card> value)
-        {
-            scoreLambda?.Invoke(value);
-        }
 
     }
 }
