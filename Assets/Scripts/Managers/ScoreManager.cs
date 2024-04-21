c97d43b0629e3116ba69d788d77675248ad7c81a
Reallife101 <ryan.ngoon@gmail.com>
2024-04-21 02:26:18 -0700

    add rest of scoring

--- Assets/Scripts/Managers/ScoreManager.cs
+++ Assets/Scripts/Managers/ScoreManager.cs
@@ -38,8 +38,13 @@ namespace VideoPoker
         {
             scoringItems.Add(new ScoringItem("Royal Flush", IsRoyalFlush, 800));
             scoringItems.Add(new ScoringItem("Straight Flush", IsStraightFlush, 50));
+            scoringItems.Add(new ScoringItem("Four of A Kind", IsFourOfKind, 25));
+            scoringItems.Add(new ScoringItem("Full House", IsFullHouse, 9));
             scoringItems.Add(new ScoringItem("Flush", IsFlush, 6));
-            scoringItems.Add(new ScoringItem("Flush", IsStraight, 4));
+            scoringItems.Add(new ScoringItem("Straight", IsStraight, 4));
+            scoringItems.Add(new ScoringItem("Three of A Kind", IsThreeOfKind, 3));
+            scoringItems.Add(new ScoringItem("Two Pairs", IsTwoPair, 2));
+            scoringItems.Add(new ScoringItem("Jacks or Better", IsJacksOrBetter, 1));
             // Add more scoring items as needed
         }
 
@@ -47,11 +52,24 @@ namespace VideoPoker
         {
             List<int> _values = new List<int>();
             List<Suit> _suits = new List<Suit>();
+            Dictionary<int, int> _valueCounts = new Dictionary<int, int>();
+
 
             foreach (Card card in hand)
             {
+                int _currentVal = card.GetValue();
                 _values.Add(card.GetValue());
                 _suits.Add(card.GetSuit());
+
+                // Add to frequency table
+                if (_valueCounts.ContainsKey(_currentVal))
+                {
+                    _valueCounts[_currentVal]++;
+                }
+                else
+                {
+                    _valueCounts[_currentVal] = 1;
+                }
             }
 
             // Sort the values in ascending order
@@ -59,7 +77,7 @@ namespace VideoPoker
 
             foreach (ScoringItem item in scoringItems)
             {
-                if (item.condition(_values, _suits))
+                if (item.condition(_values, _suits, _valueCounts))
                 {
                     Debug.Log("Scored " + item.points + " points for " + item.name);
                     break;
@@ -67,7 +85,7 @@ namespace VideoPoker
             }
         }
 
-        private bool IsRoyalFlush(List<int> values, List<Suit> suits)
+        private bool IsRoyalFlush(List<int> values, List<Suit> suits, Dictionary<int, int> valueCounts)
         {
 
             List<int> _expectedValue = new List<int> { 1, 10, 11, 12, 13 };
@@ -80,15 +98,15 @@ namespace VideoPoker
                 }
             }
 
-            return IsFlush(values,suits);
+            return IsFlush(values,suits, valueCounts);
         }
 
-        private bool IsStraightFlush(List<int> values, List<Suit> suits)
+        private bool IsStraightFlush(List<int> values, List<Suit> suits, Dictionary<int, int> valueCounts)
         {
-            return IsFlush(values, suits) && IsStraight(values, suits);
+            return IsFlush(values, suits, valueCounts) && IsStraight(values, suits, valueCounts);
         }
 
-        private bool IsStraight(List<int> values, List<Suit> suits)
+        private bool IsStraight(List<int> values, List<Suit> suits, Dictionary<int, int> valueCounts)
         {
 
             int _expectedValue = values[0] + 1;
@@ -105,7 +123,7 @@ namespace VideoPoker
             return true;
         }
 
-        private bool IsFlush(List<int> values, List<Suit> suits)
+        private bool IsFlush(List<int> values, List<Suit> suits, Dictionary<int, int> valueCounts)
         {
             for (int i = 1; i < suits.Count; i++)
             {
@@ -116,6 +134,87 @@ namespace VideoPoker
             }
 
             return true;
+
+        }
+        
+        private bool IsFourOfKind(List<int> values, List<Suit> suits, Dictionary<int, int> valueCounts)
+        {
+            foreach (var count in valueCounts.Values)
+            {
+                if (count == 4)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+
+        }
+        
+        private bool IsThreeOfKind(List<int> values, List<Suit> suits, Dictionary<int, int> valueCounts)
+        {
+            foreach (var count in valueCounts.Values)
+            {
+                if (count == 3)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+
+        }
+
+        private bool IsPair(List<int> values, List<Suit> suits, Dictionary<int, int> valueCounts)
+        {
+            foreach (var count in valueCounts.Values)
+            {
+                if (count == 2)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+
+        }
+
+        private bool IsFullHouse(List<int> values, List<Suit> suits, Dictionary<int, int> valueCounts)
+        {
+
+            return IsPair(values, suits, valueCounts) && IsThreeOfKind(values, suits, valueCounts);
+
+        }
+
+        private bool IsTwoPair(List<int> values, List<Suit> suits, Dictionary<int, int> valueCounts)
+        {
+            int pairCount = 0;
+            foreach (var count in valueCounts.Values)
+            {
+                if (count == 2)
+                {
+                    pairCount++;
+                }
+            }
+
+            return pairCount>=2;
+
+        }
+
+        private bool IsJacksOrBetter(List<int> values, List<Suit> suits, Dictionary<int, int> valueCounts)
+        {
+            foreach (KeyValuePair<int, int> pair in valueCounts)
+            {
+                if (pair.Value == 2)
+                {
+                    if (pair.Key>=11 || pair.Key==1)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
 
         }
 
--- Assets/Scripts/ScoringItem.cs
+++ Assets/Scripts/ScoringItem.cs
@@ -7,14 +7,15 @@ namespace VideoPoker
     public class ScoringItem
     {
         public string name; // Name of the scoring item
-        public Func<List<int>, List<Suit>, bool> condition; // Condition to check for scoring
+        public Func<List<int>, List<Suit>, Dictionary<int, int>, bool> condition; // Condition to check for scoring
         public int points; // Points awarded if condition is met
 
-        public ScoringItem(string _name, Func<List<int>, List<Suit>, bool> _condition, int _points)
+        public ScoringItem(string _name, Func<List<int>, List<Suit>, Dictionary<int, int>, bool> _condition, int _points)
         {
             name = _name;
             condition = _condition;
             points = _points;
         }
+
     }
 }
